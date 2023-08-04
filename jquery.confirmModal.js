fdb081faa8a56e51d97f28dfc5b03d22e1c177a7
dinhvuqn <33476398+dinhvuqn@users.noreply.github.com>
2023-08-04 14:24:39 +0700

    Add files via upload

--- jquery.confirmModal.js
+++ jquery.confirmModal.js
@@ -1,9 +1,3 @@
-/*!
- * jQuery.confirmModal v1.0
- * Copyright (c) 2018 Trim C.
- * Released under the MIT license
- * Description : simple to use plugin replacing the browser's default confirm box with bootstrap 4 modal
- */
 (function ($) {
     $currentModalTarget = {};
     $defaultsConfirmModal = {};
@@ -15,19 +9,33 @@
         if (settings === undefined) {
             var modalBoxWidth = 'auto';
             var modalVerticalCenter = '';
+            var modalScrollable = '';
+            var modalSize = '';
             var fadeAnimation = '';
             var confirmButton = 'OK';
+            var confirmTypeButton = 'btn-primary';
+            var cancelTypeButton = 'btn-secondary'
             var cancelButton = 'Cancel';
         } else {
-            if ($defaultsConfirmModal.confirmButton === undefined) {
+            if (settings.confirmButton === undefined) {
                 var confirmButton = 'OK';
             } else {
-                var confirmButton = $defaultsConfirmModal.confirmButton;
+                var confirmButton = settings.confirmButton;
             }
-            if ($defaultsConfirmModal.cancelButton === undefined) {
+            if (settings.confirmTypeButton === undefined) {
+                var confirmTypeButton = 'btn-primary';
+            } else {
+                var confirmTypeButton = settings.confirmTypeButton;
+            }
+            if (settings.cancelButton === undefined) {
                 var cancelButton = 'Cancel'; 
             } else {
-                var cancelButton = $defaultsConfirmModal.cancelButton;
+                var cancelButton = settings.cancelButton;
+            }
+            if (settings.cancelTypeButton === undefined) {
+                var cancelTypeButton = 'btn-secondary';
+            } else {
+                var cancelTypeButton = settings.cancelTypeButton;
             }
             if (settings.modalBoxWidth === undefined) {
                 var modalBoxWidth = 'auto';
@@ -38,6 +46,16 @@
                 var modalVerticalCenter = '';
             } else {
                 var modalVerticalCenter = 'modal-dialog-centered';
+            }
+            if (settings.modalScrollable === undefined || settings.modalScrollable === false) {
+                var modalScrollable = '';
+            } else {
+                var modalScrollable = 'modal-dialog-scrollable';
+            }
+            if (settings.modalSize === undefined) {
+                var modalSize = '';
+            } else {
+                var modalSize = settings.modalSize;
             }
             if (settings.messageHeader === undefined || settings.messageHeader === '') {
                 var messageHeader = '&nbsp;';
@@ -58,7 +76,7 @@
                 } else {
                     var blurSize = '0.1rem';
                 }
-                var elements = settings.backgroundBlur[0];                
+                var elements = settings.backgroundBlur[0];
                 $(elements).attr('style', '-webkit-filter: blur(' + blurSize + '); -moz-filter: blur(' + blurSize + '); -o-filter: blur(' + blurSize + '); -ms-filter: blur(' + blurSize + '); filter: blur(' + blurSize + ');');
                 $(document).one('hide.bs.modal', '.modalConfirm', function () { $(elements).removeAttr('style'); });
             }
@@ -69,16 +87,16 @@
         
         var html = `
             <div style="z-index: 5000;" class="modal ` + fadeAnimation + ` modalConfirm" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
-                <div style="max-width: ` + modalBoxWidth + `;" class="modal-dialog ` + modalVerticalCenter + `" role="document">
+                <div style="max-width: ` + modalBoxWidth + `;" class="modal-dialog ` + modalVerticalCenter + ` ` + modalScrollable + ` ` + modalSize + `" role="document">
                     <div class="modal-content">
-                        <div style="padding: 0.6rem" class="modal-header">
+                        <div class="modal-header">
                             <h6 class="modal-title">` + messageHeader + `</h6>
                             <button type="button" style="padding: 0.45rem 0.8rem;" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         </div>
-                        <div style="padding: 0.8rem; font-size: 0.9rem;" class="modal-body text-break">` + message + `</div>
+                        <div class="modal-body text-break">` + message + `</div>
                         <div style="padding: 0.55rem; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;" class="modal-footer">
-                            <button type="button" style="` + (confirmButton === 'OK' ? `width: 66px;` : ``) + ` padding: .18rem .5rem;" class="confirmButton btn btn-primary btn-sm">` + confirmButton + `</button>
-                            <button type="button" style="` + (cancelButton === 'Cancel' ? `width: 66px;` : ``) + ` padding: .18rem .5rem;" class="btn btn-secondary btn-sm" data-dismiss="modal">` + cancelButton + `</button>
+                        <button type="button" class="btn btn-sm ` + cancelTypeButton + `" data-dismiss="modal">` + cancelButton + `</button>
+                        <button type="button" class="btn btn-sm ` + confirmTypeButton + `"> ` + confirmButton + `</button>
                         </div>
                     </div>
                 </div>
@@ -88,17 +106,17 @@
         if ($.isEmptyObject($currentModalTarget)) {
             $currentModalTarget = targetElement;
             $('body').prepend(html);
-            $('.modalConfirm').modal('show');
+            $('.modalConfirm').modal('show'); // bootstrap 4
             $('.confirmButton').on('click', function(e) {
                 e.preventDefault();
-                $('.modalConfirm').modal('hide');
+                $('.modalConfirm').modal('hide');// bootstrap 4
                 if (typeof(callback) === 'function') {
                     callback(targetElement);
                 } else {
                     options(targetElement)
                 }
             });
-        } else if ($currentModalTarget.className != targetElement.className) {
+        } else if ($currentModalTarget.className != targetElement.className) {// cai thien so sanh class name
             $('body > div.modalConfirm').remove();
             $currentModalTarget = targetElement;
             $('body').prepend(html);
