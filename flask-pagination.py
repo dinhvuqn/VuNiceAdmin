ecc7f7e46b617ba58698a5a6bd3c5a0eaf2c9e9f
dinhvuqn <33476398+dinhvuqn@users.noreply.github.com>
2023-08-24 12:04:36 +0700

    Update flask-pagination.py

--- flask-pagination.py
+++ flask-pagination.py
@@ -1,28 +1,15 @@
-from flask import Blueprint
-from flask_paginate import Pagination, get_page_parameter
-
-mod = Blueprint('users', __name__)
-
-
-@mod.route('/')
-def index():
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-
-    users = User.find(...)
-    pagination = Pagination(page=page, total=users.count(), search=search, record_name='users')
-    # 'page' is the default name of the page parameter, it can be customized
-    # e.g. Pagination(page_parameter='p', ...)
-    # or set PAGE_PARAMETER in config file
-    # also likes page_parameter, you can customize for per_page_parameter
-    # you can set PER_PAGE_PARAMETER in config file
-    # e.g. Pagination(per_page_parameter='pp')
-
-    return render_template('users/index.html',
-                           users=users,
-                           pagination=pagination,
-                           )
+.pagination-page-info {
+    padding: .6em;
+    padding-left: 0;
+    width: 40em;
+    margin: .5em;
+    margin-left: 0;
+    font-size: 12px;
+}
+.pagination-page-info b {
+    color: black;
+    background: #6aa6ed;
+    padding-left: 2px;
+    padding: .1em .25em;
+    font-size: 150%;
+}
