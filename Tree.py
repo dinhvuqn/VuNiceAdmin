5e9adb5165d640578f560b997b80f28e8531c729
dinhvuqn <33476398+dinhvuqn@users.noreply.github.com>
2024-05-27 14:52:41 +0700

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -7,13 +7,15 @@ def close_alert(hwnd):
         warnings.filterwarnings('ignore')
         app = Application().connect(handle=hwnd)
         window = app.window(handle=hwnd)
-        window.close()
+        print(window)
+        window.set_focus()
+        yesBtn = window[u'&OK'] 
+        yesBtn.click()
         return True
     except Exception as e:
+        print(e)
         return False
 
 # Đóng cửa sổ alert với handle đã xác định
-hwnd_alert = 527214  # Thay thế bằng handle thực của cửa sổ alert
+hwnd_alert = 723962  # Thay thế bằng handle thực của cửa sổ alert
 close_alert(hwnd_alert)
-#https://pypi.org/project/pywinauto/0.6.8/#files
-#https://pypi.org/project/comtypes/#files
