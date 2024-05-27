88c0d3560f95b012578e586dd660c288fd3932e5
dinhvuqn <33476398+dinhvuqn@users.noreply.github.com>
2024-05-27 14:12:20 +0700

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -16,3 +16,4 @@ def close_alert(hwnd):
 hwnd_alert = 527214  # Thay thế bằng handle thực của cửa sổ alert
 close_alert(hwnd_alert)
 #https://pypi.org/project/pywinauto/0.6.8/#files
+#https://pypi.org/project/comtypes/#files
